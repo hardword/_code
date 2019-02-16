2a4861e282ed5ceebf423ad90f24e63fbc1d8c3a
hardword <hardword.sheen@gmail.com>
2019-02-16 00:17:38 -0800

    Let's keep coding
    Let's keep coding

--- _HR/Python/xml_fun.py
+++ _HR/Python/xml_fun.py
@@ -19,3 +19,29 @@ if __name__ == '__main__':
     tree = etree.ElementTree(etree.fromstring(xml))
     root = tree.getroot()
     print get_attr_number(root)
+
+
+# XML 2 - Find the Maximum Depth
+
+
+maxdepth = 0
+
+
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    level += 1
+    if level >= maxdepth:
+        maxdepth = level
+    for child in elem:
+        depth(child, level)  # Need to go deeper on Recursive
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    xml = ""
+    for i in range(n):
+        xml = xml + raw_input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print maxdepth
